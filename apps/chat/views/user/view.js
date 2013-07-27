9f25435075bb20eecf089f397a9667b9495fe2d8
Gaurav Jain <gaurav@readybytes.in>
2013-07-27 13:15:15 +0530

    tmpl rendering is done

--- apps/chat/views/user/view.js
+++ apps/chat/views/user/view.js
@@ -1,5 +1,20 @@
 var user = exports.user = {};
 
-user.display = function(app){
-	return 'qwerty';
+user.display = function(app){	
+	var jade = require('jade');	
+	
+	var options = {
+					users :  {
+		    				tj: { age: 23, email: 'tj@vision-media.ca', isA: 'human' },
+		    				tobi: { age: 1, email: 'tobi@is-amazing.com', isA: 'ferret' }
+		  					},
+		  			aname : 'Gaurav'
+	};
+	
+	var return_html = '';
+	jade.renderFile(__dirname + '/tmpl/display.jade', options, function (err, html) {
+		return_html = html;
+	});
+	
+	return return_html;
 };
